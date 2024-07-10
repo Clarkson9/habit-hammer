c98a560f621fe53b64f7056787016522d81d5d0c
Alex <alexclarkson9@yahoo.com>
2024-07-10 07:00:11 -0400

    add habit list page scaffold and start on add new habit modal scaffold

--- src/pages/HabitList/HabitList.js
+++ src/pages/HabitList/HabitList.js
@@ -1,7 +1,78 @@
 import "./HabitList.scss";
 
+import { useState, useEffect, useRef } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+import Habit from "../../components/Habit/Habit.js";
+
 const HabitList = () => {
-	return <>This is the list page</>;
+	const [habitList, setHabitList] = useState([]);
+
+	const token = localStorage.getItem("authToken");
+	useEffect(() => {
+		axios
+			.get("http://localhost:8080/habit", {
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			})
+			.then((response) => console.log(response.data));
+	}, []);
+
+	const addHabitModal = useRef(null);
+
+	const toggleModal = (ref) => {
+		ref.current.hasAttribute("open")
+			? ref.current.close()
+			: ref.current.showModal();
+	};
+
+	return (
+		<main className="center-box">
+			{/* <Habit /> */}
+
+			{/* on click trigger modal */}
+			<button
+				type="button"
+				className="button"
+				onClick={() => toggleModal(addHabitModal)}>
+				+ Add a new habit
+			</button>
+			<dialog
+				className="add-habit-modal"
+				ref={addHabitModal}
+				onClick={(e) => {
+					if (e.currentTarget === e.target) {
+						toggleModal(addHabitModal);
+					}
+				}}>
+				<form className="modal-form">
+					<h2>Add a new habit to your list</h2>
+					<input
+						type="text"
+						name="habit_name"
+						className="modal-form__input"
+						// onChange={handleChangeState}
+						// value={formValues.email}
+						placeholder="Habit"></input>
+					<input
+						type="text"
+						name="habit_why"
+						className="modal-form__input"
+						// onChange={handleChangeState}
+						// value={formValues.email}
+						placeholder="Why"></input>
+					<div className="button-wrapper">
+						<button type="button" onClick={() => toggleModal(addHabitModal)}>
+							Cancel
+						</button>
+						<button type="submit">+ Add new habit</button>
+					</div>
+				</form>
+			</dialog>
+		</main>
+	);
 };
 
 export default HabitList;
