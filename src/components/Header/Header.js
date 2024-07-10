db6cd976df87cb8327298626f61c085ffd6658fe
Alex <alexclarkson9@yahoo.com>
2024-07-09 21:24:38 -0400

    add starter home page scaffold

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,29 +1,46 @@
 import "./Header.scss";
 
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 
 const Header = () => {
 	let navigate = useNavigate();
+	const location = useLocation();
 
 	const handleNavigate = (endpoint) => {
 		navigate(endpoint);
 	};
 
-	return (
+	// ***** add handleLogout function *****
+
+	return location.pathname.includes("list") ? (
+		<div>
+			<div className="header">
+				<Link to="/">Habit Hammer</Link>
+				<button
+					type="button"
+					className="button"
+					onClick={() => handleNavigate("/")}>
+					<p className="button__text">Logout</p>
+				</button>
+			</div>
+		</div>
+	) : (
 		<div className="header">
 			<Link to="/">Habit Hammer</Link>
-			<button
-				type="button"
-				className="button"
-				onClick={() => handleNavigate("/signup")}>
-				<p className="button__text">Signup</p>
-			</button>
-			<button
-				type="button"
-				className="button"
-				onClick={() => handleNavigate("/login")}>
-				<p className="button__text">Login</p>
-			</button>
+			{/* <div className="button-wrapper">
+				<button
+					type="button"
+					className="button"
+					onClick={() => handleNavigate("/signup")}>
+					<p className="button__text">Signup</p>
+				</button>
+				<button
+					type="button"
+					className="button"
+					onClick={() => handleNavigate("/login")}>
+					<p className="button__text">Login</p>
+				</button> 
+			</div>*/}
 		</div>
 	);
 };
