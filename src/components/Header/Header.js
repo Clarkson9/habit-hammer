70ed95cfd6012e75e7cdfde3d5edb87dd8fff9d8
Alex <alexclarkson9@yahoo.com>
2024-07-09 20:54:57 -0400

    change registration url endpoint and component to Signup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,31 @@
 import "./Header.scss";
 
+import { Link, useNavigate } from "react-router-dom";
+
 const Header = () => {
-	return <></>;
+	let navigate = useNavigate();
+
+	const handleNavigate = (endpoint) => {
+		navigate(endpoint);
+	};
+
+	return (
+		<div className="header">
+			<Link to="/">Habit Hammer</Link>
+			<button
+				type="button"
+				className="button"
+				onClick={() => handleNavigate("/signup")}>
+				<p className="button__text">Signup</p>
+			</button>
+			<button
+				type="button"
+				className="button"
+				onClick={() => handleNavigate("/login")}>
+				<p className="button__text">Login</p>
+			</button>
+		</div>
+	);
 };
 
 export default Header;
